0d535f2b38a33857630b3f48f35f5254ad8410b0
liuqi <night_fightina@163.com>
2020-01-22 18:24:21 +0800

    add all

--- src/main.go
+++ src/main.go
@@ -1,61 +1,13 @@
 package main
 
 import (
-    "net/http"
-    "io/ioutil"
-    "fmt"
-    "regexp"
+    "zhenai/parser"
+    "engine"
 )
 
 func main() {
-    resp, err := http.Get(
-        "http://www.zhenai.com/zhenghun")
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
-    if resp.StatusCode != http.StatusOK {
-        fmt.Println("Error: status code", resp.StatusCode)
-        return
-    }
-    // 1.utf8Reader := transform.NewReader(resp.Body,
-    //         simplifiedchinese.GBK.NewDecoder())
-    // 2.自动确认编码
-    // e := determineEncoding(resp.Body)
-    // utf8Reader := transform.NewReader(resp.Body,
-    //         e.NewDecoder())
-    all, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    //fmt.Printf("%s\n", all)
-    printCityList(all)
+    engine.Run(engine.Request{
+        Url: "http://www.zhenai.com/zhenghun",
+        ParserFunc: parser.ParseCityList,
+    })
 }
-
-func printCityList(contents []byte) {
-    re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
-    //matches := re.FindAll(contents, -1)
-    //for _,m := range matches {
-    //    fmt.Printf("%s\n", m)
-    //}
-    matches := re.FindAllSubmatch(contents, -1)
-    for _, m := range matches {
-//        for _, subMatch := range m {
-//            fmt.Printf("%s", subMatch)
-//        }
-//        fmt.Println()
-        fmt.Printf("City:%s, URL:%s\n", m[2], m[1])
-    }
-    fmt.Printf("Matches found:%d \n", len(matches))
-}
-
-//func determineEncoding(
-//    r io.Reader) encoding.Encoding {
-//    bytes, err := bufio.NewReader(r).Peek(1024)
-//    if err != nil {
-//        panic(err)
-//    }
-//    e, _, _ := charset.DetermineEncoding(
-//        bytes, "") 
-//    return e
-//}
